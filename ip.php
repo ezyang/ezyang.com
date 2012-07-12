c8f78a7fa6f2af87707de5c5e34a7ed7e38d0ae1
Edward Z. Yang <ezyang@mit.edu>
2012-07-12 00:26:59 -0400

    Proper email address.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- ip.php
+++ ip.php
@@ -6,7 +6,7 @@ if (isset($_GET['record'])) {
     $new_ip = $_SERVER['REMOTE_ADDR'];
     if ($new_ip !== $ip) {
         mail(
-            'Edward Z. Yang <edwardzyang@thewritingpot.com>',
+            'Edward Z. Yang <ezyang@mit.edu>',
             "IP change notification ($new_ip)",
             "This is an automated notification indicating that your IP address\r\n".
             "has changed from $ip to $new_ip."
